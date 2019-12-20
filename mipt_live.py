9a1f3dab3b98e311cd97f3730caca05ca9e3de6e
lebedeva-anna <frol0wa.k5en@yandex.ru>
2019-12-20 14:06:14 +0300

    stage2.0

--- mipt_live.py
+++ mipt_live.py
@@ -2,9 +2,9 @@
 from obj import Player, Mob, Bullet
 from obj import *
 from os import path
+import math
 
 pygame.display.set_caption("Та-да-да-дам")
-clock = pygame.time.Clock()
 
 pygame.init()
 pygame.mixer.init()
@@ -13,46 +13,86 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 background = pygame.image.load(path.join(img_dir, "gametime.jpg")).convert()
 background_rect = background.get_rect()
 
-
-
-for i in range(8):
-    m = Mob()
-    all_sprites.add(m)
-    mobs.add(m)
-
-k = 0
+clock = pygame.time.Clock()
 
 running = True
+timer = 0
 
- 
+def draw_hp_bar(surf, x, y, color, hp):
+    if hp < 0:
+        hp = 0
+    bar_length = 100
+    bar_height = 10
+    fill = (hp / 100) * bar_length
+    outline_rect = pygame.Rect(x, y, bar_length, bar_height)
+    fill_rect = pygame.Rect(x, y, fill, bar_height)
+    pygame.draw.rect(surf, color, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+    
+def flux():
+    mob = Mob(mob_img2)
+    r = ((player.rect.x - boss.rect.x)**2 + (player.rect.y - boss.rect.y)**2 )**0.5
+    a = math.asin((player.rect.x - boss.rect.x) / r)
+    mob.speedx = 8 * math.sin(a)
+    mob.speedy = 8 * math.cos(a)
+    all_sprites.add(mob)
+    mobs.add(mob)
+    
+
 
 while running:
+    
     clock.tick(FPS)
+    if boss.hp >= 76 and boss.hp >= 0:
+        timer = (timer + 1) % 15
+        if (timer == 0):
+            shoot()
+    elif boss.hp <= 75 and boss.hp >= 0:
+        timer = (timer + 1) % 10
+        if (timer == 0):
+            flux()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 player.shoot()
-
+        if event.type == pygame.QUIT:
+                running = False
+                
+    
+    
     all_sprites.update()
-
+    
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    
+    hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
     for hit in hits:
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
-
-
-    hits = pygame.sprite.spritecollide(player, mobs, False)
-    if hits:   
-        running = False
-
-
+        player.hp -= 5
+        if player.hp <= 0:
+            running = False
+    
+    hits = pygame.sprite.spritecollide(boss, bullets, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        pain_snd.play()
+        boss.hp -= 25
+     
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
-
+    
+    draw_hp_bar(screen, WIDTH/2 - 50, 585 , RED, player.hp)
+    if boss.hp >= 76:
+        d = 0
+        draw_hp_bar(screen, WIDTH/2 - 50, 10 , GREEN, boss.hp)
+    elif boss.hp <= 75:
+        if d == 0:
+            stage2_snd.play()
+            d = 1 
+        draw_hp_bar(screen, WIDTH/2 - 50, 10 , YELLOW, boss.hp)
+    
+    
+    
     pygame.display.flip()
 
 pygame.quit()
--- obj.py
+++ obj.py
@@ -8,23 +8,33 @@ FPS = 60
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 player_img = pygame.image.load(path.join(img_dir, "student.png")).convert()
-mob_img = pygame.image.load(path.join(img_dir, "water.png")).convert()
+mob_img1 = pygame.image.load(path.join(img_dir, "water.png")).convert()
+mob_img2 = pygame.image.load(path.join(img_dir, "water2.png")).convert()
 bullet_img = pygame.image.load(path.join(img_dir, "bull.png")).convert()
 boss_img = pygame.image.load(path.join(img_dir, "boss.png")).convert()
+
+shoot_snd = pygame.mixer.Sound(path.join(snd_dir, 'water.wav'))
+shoot_snd.set_volume(0.2)
+pain_snd = pygame.mixer.Sound(path.join(snd_dir, 'pain.wav'))
+stage2_snd = pygame.mixer.Sound(path.join(snd_dir, 'stage2.wav'))
 
 all_sprites = pygame.sprite.Group()
 
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
-
 
 
 class Player(pygame.sprite.Sprite):
@@ -34,28 +44,46 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (100, 80))
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
+        self.radius = 40
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 5
         self.speedx = 0
+        self.speedy = 0
+        self.hp = 100
 
     def update(self):
         self.speedx = 0
+        self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
             self.speedx = -8
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
         self.rect.x += self.speedx
+        
+        if keystate[pygame.K_UP]:
+            self.speedy = -8
+        
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 8
+        
+        self.rect.y += self.speedy
+        
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        
+        if self.rect.top > HEIGHT - 100:
+            self.rect.top = HEIGHT - 100
+        if self.rect.bottom < 100:
+            self.rect.bottom = 100
 
     def shoot(self):    
         bullet = Bullet( self.rect.centerx,  self.rect.top)
-        print(bullet.rect.bottom)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        
 
 player = Player()
 all_sprites.add(player)
@@ -68,39 +96,48 @@ class Boss(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.top = 100
-        self.speedy = random.randrange(1, 4)
-        self.speedx = 0
+        self.speedy = random.randrange(-4 , 4)
+        self.speedx = random.randrange(-4 , 4)
+        self.radius = 50
+        self.hp = 100
         
-    def update(self):
-        self.speedx = player.speedx 
+    def update(self): 
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > 150 or self.rect.bottom < 30:
+        
+        if self.rect.top > 150 or self.rect.bottom < 120:
             self.speedy = - self.speedy
+        if self.rect.left < -30 or self.rect.right > 540:
+            self.speedx = - self.speedx
+        if boss.hp < 0:
+            boss.kill()
+def shoot():
+    mob = Mob(mob_img1)
+    all_sprites.add(mob)
+    mobs.add(mob)
+    shoot_snd.play()
 
 
 boss = Boss()
 all_sprites.add(boss)
 
 class Mob(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
-        self.image =  pygame.transform.scale(mob_img, (50, 38))
+        self.image =  pygame.transform.scale(img, (50, 38))
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
-        self.rect.x = boss.rect.x
-        self.rect.y = boss.rect.y
+        self.rect.x = boss.rect.x + 60
+        self.rect.y = boss.rect.y + 95
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
-
+        self.radius = 30
     def update(self):
         
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 8)
+            self.kill()
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -114,7 +151,8 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.bottom = y
         self.rect.centerx = x
         self.speedy = -10
-
+        self.radius = 10
+        
     def update(self):
         self.rect.y += self.speedy
         if self.rect.bottom < 0:
